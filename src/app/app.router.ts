f8c5eba988237043b75a8b01d0317af8cae01b8f
Jefferson Lucas <jefferson@dimensiva.online>
2019-05-13 11:32:03 -0300

    criando rotas filhas

--- src/app/app.router.ts
+++ src/app/app.router.ts
@@ -3,13 +3,21 @@ import {HomeComponent} from './home/home.component';
 import {AboutComponent} from './about/about.component';
 import {RestaurantsComponent} from './restaurants/restaurants.component';
 import {RestaurantsDetailComponent} from './restaurants-detail/restaurants-detail.component';
+import {MenuComponent} from './restaurants-detail/menu/menu.component';
+import {ReviewsComponent} from './restaurants-detail/reviews/reviews.component';
 
 
 export const ROUTES: Routes = [
   {path: '', component: HomeComponent},
   {path: 'about', component: AboutComponent},
   {path: 'restaurantes', component: RestaurantsComponent},
-  {path: 'restaurantes/:id', component: RestaurantsDetailComponent}
+  {path: 'restaurantes/:id', component: RestaurantsDetailComponent,
+    children: [
+      {path: '', redirectTo: 'menu', pathMatch: 'full'},
+      {path: 'menu', component: MenuComponent},
+      {path: 'reviews', component: ReviewsComponent}
+
+    ]}
 
 ];
 
