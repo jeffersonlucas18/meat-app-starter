b640de530ff91df074d340d86d466b61cf5b8a10
Jefferson Lucas <jefferson@dimensiva.online>
2019-05-15 11:48:22 -0300

    CONCERTANDO A TABELA DE COMPRAS

---
+++ src/app/order/order-tens/order-tens.component.ts
@@ -0,0 +1,31 @@
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {CartItemModel} from '../../restaurants-detail/shopping-cart/cart-item.model';
+
+@Component({
+  selector: 'mt-order-tens',
+  templateUrl: './order-tens.component.html'
+})
+export class OrderTensComponent implements OnInit {
+
+  @Input() itens:CartItemModel[];
+
+  @Output() increaseqTY = new EventEmitter<CartItemModel>();
+  @Output() desincreaseqTY = new EventEmitter<CartItemModel>();
+  @Output() remove = new EventEmitter<CartItemModel>();
+
+  constructor() { }
+
+
+  ngOnInit() {
+  }
+
+  emitIncreaseQty(item: CartItemModel) {
+    this.increaseqTY.emit(item);
+  }
+  emitDescreaseQty(item: CartItemModel) {
+    this.desincreaseqTY.emit(item);
+  }
+  emitRemove(item: CartItemModel) {
+    this.remove.emit(item);
+  }
+}
